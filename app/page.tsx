89d370b769e315a93959cf1fb2c201e3022e1624
Muhammad Waqas <mwaqas4296@gmail.com>
2026-07-19 16:29:18 +0500

    Updated website content

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
     <>
       <Hero />
       <Stats />
-      <LogoCloudSection />
+      {/* <LogoCloudSection /> */}
       <ServicesPreview />
       <ProcessSection />
       <TestimonialsSection />
